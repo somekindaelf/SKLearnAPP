c61b805091f86150e53013e4169ff126771ca165
Elf <ewan.k@live.com>
2024-08-14 13:21:14 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    REACT_APP_FIREBASE_MEASUREMENT_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,  // Correctly assign the measurement ID
 };
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
