3368032c1bcad7ce2b5daadb0d0238512db0d86c
Elf <ewan.k@live.com>
2024-08-14 13:08:41 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 
 // Firebase configuration (replace with your Firebase config)
 const firebaseConfig = {
-    apiKey: "AIzaSyCPptntIlqX404tnvfzSukt-wF8hG2584k",
-    authDomain: "sklearnapp.firebaseapp.com",
-    projectId: "sklearnapp",
-    storageBucket: "sklearnapp.appspot.com",
-    messagingSenderId: "767064605163",
-    appId: "1:767064605163:web:fa06f4a463a4408666d9ae",
-    measurementId: "G-335VJC09CJ"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
